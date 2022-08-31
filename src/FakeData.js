307e4042293a9f58845a009a54e8588afdbd0415
idkjay <jordanchu95@gmail.com>
2022-08-31 15:51:40 -0400

    basic app practice

---
+++ src/FakeData.js
@@ -0,0 +1,22 @@
+export const UsersData = [
+	{
+		id: 1,
+		name: "Jay Yoon",
+		username: "jihoeyoon"
+	},
+	{
+		id: 2,
+		name: "Tony Zheng",
+		username: "deathgod"
+	},
+	{
+		id: 3, 
+		name: "Michael Ahn",
+		username: "ahnboy"
+	},
+	{
+		id: 4,
+		name: "Erin Zeng",
+		username: "36km"
+	}
+]
